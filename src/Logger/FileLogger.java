3c609fb25335e8f87ed0955992d36dc5c3904182
Victor Olave <victorolave1131@gmail.com>
2019-08-07 22:40:58 -0500

    Fake File

--- src/Logger/FileLogger.java
+++ src/Logger/FileLogger.java
@@ -2,9 +2,8 @@ package Logger;
 
 public class FileLogger implements Logger {
     public void log(String msg) {
-//        FileUtil futil = new FileUtil();
-//        futil.writeToFile("log.txt", msg, true, true);
-        System.out.println("By File: " + msg);
+        FileUtil fileUtil = new FileUtil();
+        fileUtil.writeToFile("log.txt", msg, true, true);
     }
 }
 
---
+++ src/Logger/FileUtil.java
@@ -0,0 +1,9 @@
+package Logger;
+
+public class FileUtil{
+
+    public void writeToFile(String file, String msg, boolean a, boolean b) {
+        System.out.println("El mensaje: " + msg + ", ha sido grabado con exito en el archivo " + file);
+    }
+
+}
